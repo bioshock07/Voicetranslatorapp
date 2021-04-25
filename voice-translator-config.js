799685d2f1e3c300dd7b0572a9d77a7660354597
Chaitanya Karadkhedkar <35321695+bioshock07@users.noreply.github.com>
2021-04-25 23:20:58 +0530

    Add files via upload

---
+++ voice-translator-config.js
@@ -0,0 +1,3 @@
+var bucketName = 'voicetranslatorapp-voicetranslatorbucket-1gnmryuohbf2v';
+var IdentityPoolId = 'us-east-1:39423518-ec73-444a-a15e-eaa9b74b2973';
+var lambdaFunction = 'VoiceTranslatorApp-VoiceTranslatorLambda-GNX8U4PX1AGV';
